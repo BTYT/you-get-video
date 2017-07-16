9a09c59868d57f41f5e1b62b9b9da6a85221a0d8
hongjie <32968210@qq.com>
2017-07-16 19:46:13 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Author: zhouhongjie@apowo.com
+# Date: 2017-07-16 15:12:36
+
+import helper
+import argparse
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--url", help = "the url of web")
+	options = parser.parse_args()
+
+	# print(options.url)
+	# http://www.soku.com/search_video/q_王者荣耀_orderby_1_limitdate_0?site=14&page=2
+	
+	pq = helper.get('http://www.soku.com/search_video/q_王者荣耀_orderby_1_limitdate_0?site=14&page=2')
+	for a in pq('.v-link > a'):
+		# print(a.get('href'))
+		helper.runCmd('you-get --format=mp4 %s' % a.get('href'))
+		break
